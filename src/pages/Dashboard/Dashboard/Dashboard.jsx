2caba362e80ffda29186af75ea7f6d2b3824b089
abuabdullah23 <abuobaidazayed@gmail.com>
2023-12-22 05:13:37 +0600

    update status 2

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div className='px-2 lg:px-4'>
-            Dashboard
+            Dashboard , All Task
         </div>
     );
 };
