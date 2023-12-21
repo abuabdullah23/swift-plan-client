a379cf9f32157355e5688ea825cb154312bbc1c8
abuabdullah23 <abuobaidazayed@gmail.com>
2023-12-22 05:07:12 +0600

    update status

---
+++ src/components/Modal/UpdateTaskModal.jsx
@@ -0,0 +1,153 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import toast from 'react-hot-toast';
+
+const UpdateTaskModal = ({ isOpen, closeModal, refetch, task }) => {
+    const { register, handleSubmit, reset } = useForm();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const onSubmit = (data) => {
+        const name = data?.name;
+        const description = data?.description;
+        const priority = data?.priority;
+        const deadline = data?.deadline;
+
+        const updateTaskInfo = {
+            userEmail: user?.email,
+            name,
+            description,
+            priority,
+            deadline
+        }
+
+        axiosSecure.put(`/update-my-task/${task?._id}`, updateTaskInfo)
+            .then(res => {
+                if (res.status === 200) {
+                    toast.success('Task added successful');
+                    refetch();
+                    closeModal();
+                    reset();
+                }
+            })
+            .catch(error => {
+                toast.error(error.message);
+            })
+    }
+
+    return (
+        <div>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-[9999]" onClose={closeModal}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/25" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl text-[var(--primary-text)] bg-[var(--body)] p-6 text-left align-middle shadow-xl border-2 border-[var(--border)] transition-all">
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="text-lg text-center font-medium leading-6"
+                                    >
+                                        Update your Task
+                                    </Dialog.Title>
+
+                                    <div>
+                                        <div className="flex flex-col gap-3 mt-2 text-base">
+
+                                            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3'>
+                                                <div className='flex flex-col items-start gap-2'>
+                                                    <label className='font-semibold' htmlFor="name">Task Name</label>
+                                                    <input
+                                                        required
+                                                        type="text"
+                                                        name='name'
+                                                        defaultValue={task?.name}
+                                                        placeholder='Task Name'
+                                                        {...register("name")}
+                                                        className='w-full py-2 px-3 bg-transparent border border-[#203c79] focus:border-[#203c79] rounded outline-none' />
+                                                </div>
+                                                <div className='flex flex-col items-start gap-2'>
+                                                    <label className='font-semibold' htmlFor="description">Description</label>
+                                                    <textarea
+                                                        required
+                                                        type="text"
+                                                        name='description'
+                                                        defaultValue={task?.description}
+                                                        placeholder='Task Description'
+                                                        {...register("description")}
+                                                        className='w-full py-2 px-3 bg-transparent border border-[#203c79] focus:border-[#203c79] rounded outline-none' />
+                                                </div>
+                                                <div className='flex flex-col items-start gap-2'>
+                                                    <label className='font-semibold' htmlFor="">Priority</label>
+                                                    <select
+                                                        required
+                                                        name="priority"
+                                                        {...register("priority")}
+                                                        className='w-full py-2 px-2 border border-[#203c79] focus:border-[#203c79] bg-[var(--body)] rounded outline-none'>
+                                                        <option value={task?.priority}>{task?.priority}</option>
+                                                        <option value="low">Low</option>
+                                                        <option value="moderate">Moderate</option>
+                                                        <option value="high">High</option>
+
+                                                    </select>
+                                                </div>
+                                                <div className='flex flex-col items-start gap-2'>
+                                                    <label className='font-semibold' htmlFor="">Deadline</label>
+                                                    <input
+                                                        required
+                                                        type='date'
+                                                        name="deadline"
+                                                        defaultValue={task?.deadline}
+                                                        {...register("deadline")}
+                                                        className='w-full py-2 px-2  bg-[var(--body)] text-[var(--primary-text)] border border-[#203c79] focus:border-[#203c79] rounded outline-none' />
+                                                </div>
+                                                <div className='flex items-center justify-between'>
+                                                    <p
+                                                        onClick={closeModal}
+                                                        className="cursor-pointer rounded-md border-2 border-[var(--border)] hover:border-[#203c79] py-2 px-3 font-semibold w-fit transition-all duration-300"
+                                                    >
+                                                        Close
+                                                    </p>
+                                                    <button
+                                                        type='submit'
+                                                        className="rounded-md bg-[var(--primary)] border-2 border-[var(--border)] text-white hover:border-[#203c79] py-2 px-3 font-semibold w-fit transition-all duration-300"
+                                                    >
+                                                        Create
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </div>
+    );
+};
+
+export default UpdateTaskModal;
--- src/pages/Dashboard/AllTask/TaskCard.jsx
+++ src/pages/Dashboard/AllTask/TaskCard.jsx
@@ -2,16 +2,24 @@ import moment from "moment";
 import { MdDateRange } from "react-icons/md";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import toast from "react-hot-toast";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { useState } from "react";
+import UpdateTaskModal from "../../../components/Modal/UpdateTaskModal";
 
 const TaskCard = ({ task, index, refetch }) => {
-    const { _id, name, userEmail, status, priority, deadline, createdAt, description } = task;
-
+    const { _id, name, userEmail, priority, deadline, createdAt, description } = task;
     const axiosSecure = useAxiosSecure();
 
-    // handle delete my article
-    const handleDeleteMyArticle = (id) => {
+    // for modal
+    const [isOpen, setIsOpen] = useState(false);
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
+
+    // handle delete my task
+    const handleDeleteMyTask = (id) => {
         Swal.fire({
             title: 'Are you sure to Delete your article?',
             icon: 'warning',
@@ -35,22 +43,59 @@ const TaskCard = ({ task, index, refetch }) => {
         })
     }
 
+
+    // handleChangeStatus
+    const handleChangeStatus = (e) => {
+        const updateStatus = e.target.value;
+        axiosSecure.put(`/update-task-status/${_id}`, { updateStatus })
+            .then(res => {
+                if (res.status === 200) {
+                    refetch();
+                    toast.success('Updated Status Successful');
+                }
+            })
+    }
+
     return (
         <div className='border border-[var(--border)] rounded relative p-3 flex flex-col gap-2 hover:shadow-lg hover:scale-[103%] duration-200'>
             <h2 className='text-lg font-semibold'>{name}</h2>
             <p>{description}</p>
 
-            <div className="flex flex-col gap-1">
-                <div className="flex items-center gap-4">
+            <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-3">
                     <p title={moment(createdAt).format("D MMMM YYYY, dddd, h:mm:ss A")} className="flex gap-2 items-center"><span><MdDateRange /></span> {moment(createdAt).format("D MMM, YYYY")}</p>
                     <button
-                        onClick={() => handleDeleteMyArticle(_id)}
-                        className='py-[2px] text-[#ff2929] rounded-sm hover:shadow-lg hover:shadow-[#ff2929]/50 flex flex-row items-center gap-1'><FaTrashAlt /> <span></span>
+                        title="Edit"
+                        onClick={() => setIsOpen(true)}
+                        className='py-[6px] px-1 text-base text-[var(--primary-text)] rounded-sm hover:shadow-lg flex flex-row items-center gap-1'><FaEdit /> <span></span>
+                    </button>
+                    <button
+                        title="Delete"
+                        onClick={() => handleDeleteMyTask(_id)}
+                        className='py-[6px] px-1 text-sm text-[var(--primary-text)] rounded-sm hover:shadow-lg flex flex-row items-center gap-1'><FaTrashAlt /> <span></span>
                     </button>
                 </div>
 
-                <p className="flex gap-2 items-center"><span className="bg-[#ff2929] text-white py-[1px] px-[6px] rounded-sm">Deadline</span> {moment(deadline).format("D MMM, YYYY")}</p>
+                <div className="flex items-center gap-3">
+                    <p className="flex gap-2 items-center"><span className="bg-[#ff2929] text-white text-sm py-[1px] px-[6px] rounded-sm">Deadline</span> {moment(deadline).format("D MMM, YYYY")}</p>
+                </div>
+                <div className="flex items-center justify-end">
+                    <select defaultValue={task?.status} onChange={handleChangeStatus} className={`text-base font-semibold bg-[var(--body)] outline-none border border-[var(--border)] rounded py-1 px-2`}>
+                        <option value="to-do">to do</option>
+                        <option value="on-going">on going</option>
+                        <option value="completed">completed</option>
+                    </select>
+                </div>
+
             </div>
+
+            <UpdateTaskModal
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                closeModal={closeModal}
+                refetch={refetch}
+                task={task}
+            />
         </div>
     );
 };
